d9955cf9cf1be718285cc51a9c19c0fe35e9c6f8
Thomas Phillips <thomas@teknique.com>
2019-09-05 21:51:44 +1200

    Support Python3
    * adds requirements and gitignore
    * Change single quotes to dual quotes

--- get-users.py
+++ get-users.py
@@ -3,10 +3,10 @@
 import os
 from requests_oauthlib import OAuth1Session
 
-client_key = os.getenv('INDIGITOUS_CLIENT_KEY')
-client_secret = os.getenv('INDIGITOUS_CLIENT_SECRET')
+client_key = os.environ['INDIGITOUS_CLIENT_KEY']
+client_secret = os.environ['INDIGITOUS_CLIENT_SECRET']
 
-request_token_url = 'https://indigitous.org/oauth1/request'
+request_token_url = "https://indigitous.org/oauth1/request"
 
 oauth = OAuth1Session(client_key, client_secret=client_secret)
 fetch_response = oauth.fetch_request_token(request_token_url)
@@ -14,13 +14,13 @@ fetch_response = oauth.fetch_request_token(request_token_url)
 resource_owner_key = fetch_response.get('oauth_token')
 resource_owner_secret = fetch_response.get('oauth_token_secret')
 
-base_authorization_url = 'https://indigitous.org/oauth1/authorize'
+base_authorization_url = "https://indigitous.org/oauth1/authorize"
 authorization_url = oauth.authorization_url(base_authorization_url)
-print 'Please go here, authorize, then export it as environment variable INDIGITOUS_VERIFIER', + authorization_url
+input("Please go here: {}, authorize, then export it as environment variable INDIGITOUS_VERIFIER and press enter".format(authorization_url))
 
-verifier = os.getenv('INDIGITOUS_VERIFIER')
+verifier = os.environ['INDIGITOUS_VERIFIER']
 
-access_token_url = 'https://indigitous.org/oauth1/access'
+access_token_url = "https://indigitous.org/oauth1/access"
 
 oauth = OAuth1Session(client_key,
                           client_secret=client_secret,
@@ -32,7 +32,7 @@ resource_owner_key = oauth_tokens.get('oauth_token')
 resource_owner_secret = oauth_tokens.get('oauth_token_secret')
 resource_owner_key, resource_owner_secret
 
-protected_url = 'https://indigitous.org/wp-json/wp/v2/users/'
+protected_url = "https://indigitous.org/wp-json/wp/v2/users/"
 
 oauth = OAuth1Session(client_key,
                           client_secret=client_secret,
